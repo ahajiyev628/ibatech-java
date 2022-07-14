feaadaad3f9ecc2030521b7de04b015f92cd9899
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-07-14 19:23:50 +0300

    envelope V6

--- src/main/java/lesson5/Envelope5.java
+++ src/main/java/lesson5/Envelope5.java
@@ -2,16 +2,17 @@ package lesson5;
 
 
 /*
- ************************
- ************************
- **  *              *  **
- **     *        *     **
- **       *    *       **
- **         X          **
- **      *      *      **
- **   *            *   **
- ************************
- ************************
+  01234567...
+0 ************************
+1 ************************
+2 **  *              *  **
+3 **     *        *     **
+4 **       *    *       **
+5 **         X          **
+6 **      *      *      **
+7 **   *            *   **
+8 ************************
+9 ************************
 
  *
  * */
@@ -29,30 +30,30 @@ public class Envelope5 {
         StringBuilder sb = new StringBuilder();
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                if (row == firstRow ||                   // firstRow
-                        row == firstRow + 1 ||
-                        col == firstCol ||
-                        col == firstCol + 1||                   // firstCol
-                        col == lastCol ||                    // lastCol
-                        row == lastRow ||                     // firstCol
-                        col == lastCol - 1||                    // lastCol
-                        row == lastRow - 1||                    // lastRow
-                        col == Math.round(row * k) ||        // diagonal 1
-                        col == lastCol - Math.round(row * k) // diagonal 2
-                )
-                    sb.append("*");
-                else
-                    sb.append(" ");
+              if (row == lastRow / 2 && col == lastCol / 2)
+                sb.append("X");
+              else if (row == firstRow ||                   // firstRow
+                  row == firstRow + 1 ||
+                  col == firstCol ||
+                  col == firstCol + 1||                   // firstCol
+                  col == lastCol ||                    // lastCol
+                  row == lastRow ||                     // firstCol
+                  col == lastCol - 1||                    // lastCol
+                  row == lastRow - 1||                    // lastRow
+                  col == Math.round(row * k) ||        // diagonal 1
+                  col == lastCol - Math.round(row * k) // diagonal 2
+              ) sb.append("*");
+              else sb.append(" ");
             }
-            sb.append("\n");
+          sb.append("\n");
         }
         return sb.toString();
     }
 
     public static void main(String[] args) {
         /** input data */
-        int height = 12;
-        int width = 22;
+        int height = 13;
+        int width = 23;
 
         /** call the business logic extracted and decoupled from input and output */
         String result = makeEnvelope(width, height);
