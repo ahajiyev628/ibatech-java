768191e86e5af2b59806281ac5a92a0db13cca19
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-07-18 19:06:30 +0300

    apples and oranges -- hackerrank

--- src/main/java/lesson6/AppleAndOranges.java
+++ src/main/java/lesson6/AppleAndOranges.java
@@ -5,12 +5,12 @@ import java.util.List;
 /** https://www.hackerrank.com/challenges/apple-and-orange/problem */
 public class AppleAndOranges {
 
-  public static int[] convert(List<Integer> xs) {
+  public static int[] convertToArray(List<Integer> xs) {
     return xs.stream().mapToInt(a -> a).toArray();
   }
 
-  static boolean inTheRange(int x, int l, int r) {
-    return x >= l && x <= r;
+  static boolean inTheRange(int pos, int l, int r) {
+    return pos >= l && pos <= r;
   }
 
   public static void countApplesAndOranges(
@@ -21,8 +21,8 @@ public class AppleAndOranges {
     List<Integer> apples0, // apples distances
     List<Integer> oranges0 // oranges distances
   ) {
-    int[] apples = convert(apples0);
-    int[] oranges = convert(oranges0);
+    int[] apples = convertToArray(apples0);
+    int[] oranges = convertToArray(oranges0);
     int ac = 0;
     int oc = 0;
     // count apples
@@ -31,8 +31,8 @@ public class AppleAndOranges {
       if (inTheRange(pos, s, t)) ac++;
     }
     // count oranges
-    for (int i = 0; i < oranges.length; i++) {
-      int pos = b + oranges[i];
+    for (int orange: oranges) {
+      int pos = b + orange;
       if (inTheRange(pos, s, t)) oc++;
     }
     System.out.println(ac);
