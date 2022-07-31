5d47a5d46a989349d38aa652c86f7928c63fd775
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-07-31 10:18:04 +0300

    list filtering

---
+++ src/main/java/lesson10/ListFiltering.java
@@ -0,0 +1,58 @@
+package lesson10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ListFiltering {
+
+  public static List<Integer> filterNegative(List<Integer> data) {
+    ArrayList<Integer> result = new ArrayList<>();
+    for (int x: data) {
+      if (x < 0) result.add(x);
+    }
+    return result;
+  }
+
+  public static List<Integer> filterNegativeExplanation(List<Integer> data) {
+    ArrayList<Integer> result = new ArrayList<>();
+
+    class NegativeProcessor implements Consumer<Integer> {
+      @Override
+      public void accept(Integer x) {
+        if (x < 0) result.add(x);
+      }
+    }
+
+    NegativeProcessor np = new NegativeProcessor();
+
+    data.forEach(x -> np.accept(x));
+
+    return result;
+  }
+
+  public static List<Integer> filterNegativeV2(List<Integer> data) {
+    ArrayList<Integer> result = new ArrayList<>();
+    data.forEach(x -> {
+      if (x < 0) result.add(x);
+    });
+    return result;
+  }
+
+  public static List<Integer> filterPositive(List<Integer> data) {
+    ArrayList<Integer> result = new ArrayList<>();
+    for (int i = 0; i < data.size(); i++) {
+      int x = data.get(i);
+      if (x > 0) result.add(x);
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    List<Integer> data = Arrays.asList(1, 4, -7, -11, 3, 8, -3);
+    System.out.println(filterNegative(data));
+    System.out.println(filterPositive(data));
+  }
+
+}
