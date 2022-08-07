609aa512090a643d380ba964ec6534fcb48701cb
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-08-07 11:18:21 +0300

    brackets

---
+++ src/main/java/lesson12/BracketsV1.java
@@ -0,0 +1,47 @@
+package lesson12;
+
+import java.util.Stack;
+
+/**
+ * {
+ *   {
+ *     <<>><>
+ *     {}{}(
+ *     {
+ *       ()
+ *     }
+ *     )
+ *   }
+ *   [][()]
+ *   ()
+ * }
+ *
+ * 1. {}
+ * 2. []
+ * 3. ()
+ * 4. <>
+ *
+ *
+ * }
+ */
+public class BracketsV1 {
+
+  public boolean isValid(String line) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < line.length(); i++) {
+      char c = line.charAt(i);
+      switch (c) {
+        case '{':
+          stack.push(c);
+          break;
+        case '}':
+          if (stack.isEmpty()) return false;
+          stack.pop(); // can produce NPX
+          break;
+        default:
+      }
+    }
+    return stack.isEmpty();
+  }
+
+}
