27e4e6298280354b900981f5cc2d5269223ca5e7
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-08-07 16:18:25 +0300

    comparator

---
+++ src/main/java/lesson12/AppleApp.java
@@ -0,0 +1,16 @@
+package lesson12;
+
+public class AppleApp {
+
+  public static void main(String[] args) {
+
+    System.out.println(
+        "Apple " +
+            "costs " +
+            + '2'
+        + " USD"
+    );
+
+  }
+
+}
