10bb565df53cd1c84b3b6ddf5bdb0c9c67248408
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-09-22 18:52:49 +0300

    lesson web 1

---
+++ src/main/java/lesson21web/ServerApp.java
@@ -0,0 +1,18 @@
+package lesson21web;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+/**
+ * https://mvnrepository.com
+ */
+public class ServerApp {
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(8080);
+    ServletContextHandler handler = new ServletContextHandler();
+    handler.addServlet(HelloServlet.class, "/hello");
+    server.setHandler(handler);
+    server.start();
+    server.join();
+  }
+}
